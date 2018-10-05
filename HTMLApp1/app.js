54c09bd15c04487516d44c8186c8ec9ea72ddc12
Will <doggy.huang@gmail.com>
2018-10-05 23:25:29 +0800

    Fix CI issue (can't build because lack of some files)

---
+++ HTMLApp1/app.js
@@ -0,0 +1,27 @@
+var Greeter = /** @class */ (function () {
+    function Greeter(element) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+    Greeter.prototype.start = function () {
+        var _this = this;
+        this.timerToken = setInterval(function () {
+            var now = new Date().toUTCString();
+            _this.span.innerHTML = now;
+            appInsights.trackEvent('Timer', { 'now': now });
+        }, 500);
+    };
+    Greeter.prototype.stop = function () {
+        clearTimeout(this.timerToken);
+    };
+    return Greeter;
+}());
+window.onload = function () {
+    var el = document.getElementById('content');
+    var greeter = new Greeter(el);
+    greeter.start();
+};
+//# sourceMappingURL=app.js.map
